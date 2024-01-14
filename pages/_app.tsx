40574518f306d1eefed214880bddff35f73fa29c
sabal-whitehat <sabalachh@gmail.com>
2024-01-13 23:58:38 -0800

    feat(home): Home page title with animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
 
+import LocalFont from "next/font/local";
+
+export const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,129 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
-  theme: {
-    extend: {
-      colors: {
-        "accent-1": "#FAFAFA",
-        "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
-      },
-      spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
-      },
-      boxShadow: {
-        sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
-        md: "0 8px 30px rgba(0, 0, 0, 0.12)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+	theme: {
+		extend: {
+			colors: {
+				"accent-1": "#FAFAFA",
+				"accent-2": "#EAEAEA",
+				"accent-7": "#333",
+				success: "#0070f3",
+				cyan: "#79FFE1",
+			},
+			spacing: {
+				28: "7rem",
+			},
+			letterSpacing: {
+				tighter: "-.04em",
+			},
+			lineHeight: {
+				tight: 1.2,
+			},
+			fontSize: {
+				"5xl": "2.5rem",
+				"6xl": "2.75rem",
+				"7xl": "4.5rem",
+				"8xl": "6.25rem",
+			},
+			boxShadow: {
+				sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
+				md: "0 8px 30px rgba(0, 0, 0, 0.12)",
+			},
+			fontFamily: {
+				display: ["var(--font-calsans)"],
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						"code::before": {
+							content: '""',
+						},
+						"code::after": {
+							content: '""',
+						},
+					},
+				},
+				quoteless: {
+					css: {
+						"blockquote p:first-of-type::before": { content: "none" },
+						"blockquote p:first-of-type::after": { content: "none" },
+					},
+				},
+			},
+			backgroundImage: {
+				"gradient-radial":
+					"radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+			},
+			animation: {
+				"fade-in": "fade-in 3s ease-in-out forwards",
+				title: "title 3s ease-out forwards",
+				"fade-left": "fade-left 3s ease-in-out forwards",
+				"fade-right": "fade-right 3s ease-in-out forwards",
+			},
+			keyframes: {
+				"fade-in": {
+					"0%": {
+						opacity: "0%",
+					},
+					"75%": {
+						opacity: "0%",
+					},
+					"100%": {
+						opacity: "100%",
+					},
+				},
+				"fade-left": {
+					"0%": {
+						transform: "translateX(100%)",
+						opacity: "0%",
+					},
+
+					"30%": {
+						transform: "translateX(0%)",
+						opacity: "100%",
+					},
+					"100%": {
+						opacity: "0%",
+					},
+				},
+				"fade-right": {
+					"0%": {
+						transform: "translateX(-100%)",
+						opacity: "0%",
+					},
+
+					"30%": {
+						transform: "translateX(0%)",
+						opacity: "100%",
+					},
+					"100%": {
+						opacity: "0%",
+					},
+				},
+				title: {
+					"0%": {
+						"line-height": "0%",
+						"letter-spacing": "0.25em",
+						opacity: "0",
+					},
+					"25%": {
+						"line-height": "0%",
+						opacity: "0%",
+					},
+					"80%": {
+						opacity: "100%",
+					},
+
+					"100%": {
+						"line-height": "100%",
+						opacity: "100%",
+					},
+				},
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("tailwindcss-debug-screens"),
+	],
 };
